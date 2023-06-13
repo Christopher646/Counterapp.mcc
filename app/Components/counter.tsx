ad5dcde7a61433c2b158f18921a32bf14fef3fb8
ConEd <coned@MCCNET.MCCNEB.EDU>
2023-06-13 12:00:27 -0500

    Finished counter app

--- app/Components/counter.tsx
+++ app/Components/counter.tsx
@@ -6,16 +6,23 @@ const Counter = () => {
 const incrementCounter = () => {
     setcount (count + 1);
 }
+const decrementcounter = () => {
+    setcount ( count - 1)
+}
+const resetcounter = () => {
+    setcount (0);
+}
+
 
     return(
         <div>
             <h1 className="componentHeading">Counter</h1>
-            <p className="counterLable">Count: {count}</p>
+            <p className={count>10?"greater-then-10":"smaller-than-10"}>Count: {count}</p>
+    
             <button className="FancyButton" onClick={() => incrementCounter()}>Increment</button>
-            <button className="FancyButton"> Decrement</button>
-            <button className="FancyButton">reset</button>
+            <button className="FancyButton" onClick={()=> decrementcounter()}>Decrement</button>
+            <button className="FancyButton"onClick={()=> resetcounter()}>reset</button>
         </div>
-ss
     )
 }
 export default Counter;
